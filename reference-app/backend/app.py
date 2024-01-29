a27a26daf49cb1a7c141c5e745c09ac106fae88d
li1515 <ihnatenkolv@gmail.com>
2024-01-29 11:48:40 +0100

    Added test app

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,22 +1,68 @@
 from flask import Flask, render_template, request, jsonify
-
+import time
+import random
 import pymongo
 from flask_pymongo import PyMongo
+from prometheus_flask_exporter import PrometheusMetrics
+from jaeger_client import Config
+import logging
+from os import getenv
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "example-mongodb"
-app.config[
-    "MONGO_URI"
-] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
+app.config['MONGO_DBNAME'] = 'example-mongodb'
+app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
+endpoint_request_counter = metrics.counter(
+    'endpoint_request_counter', 'Request count by request endpoint',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+JAEGER_AGENT_HOST = getenv('JAEGER_AGENT_HOST', 'localhost')
+
+class InvalidHandle(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        error_message = dict(self.payload or ())
+        error_message['message'] = self.message
+        return error_message
+
+@app.errorhandler(InvalidHandle)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+    
 @app.route("/")
+@endpoint_request_counter
 def homepage():
-    return "Hello World"
+    with tracer.start_span('hello-world'):
+        return "Hello World"
 
+@app.route('/error')
+@endpoint_request_counter
+def get_error():
+    raise InvalidHandle('error occur', status_code=410)
 
 @app.route("/api")
 def my_api():
--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,11 +1,74 @@
 from flask import Flask, render_template, request
 
+import threading
+import requests
+import random
+import time
+from prometheus_flask_exporter import PrometheusMetrics
+import logging
+from jaeger_client import Config
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, group_by='endpoint')
+
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
+
+endpoint_request_counter = metrics.counter(
+    'endpoint_request_counter', 'Request count by request endpoint',
+    labels={'endpoint': lambda: request.endpoint}
+)
+endpoints = ('error', 'foo', 'healthz')
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('frontend')
+
+def random():
+    while True:
+        try:
+            target = random.choice(endpoints)
+            requests.get("http://app:8081/%s" % target, timeout=1)
+
+        except:
+            pass
 
 
-@app.route("/")
+@app.route('/')
+@endpoint_request_counter
 def homepage():
     return render_template("main.html")
+    with tracer.start_span('random') as span:
+        threading.Thread(target=random).start()
+        for _ in range(4):
+            thread = threading.Thread(target=random)
+            thread.daemon = True
+            thread.start()
+
+        while True:
+            time.sleep(1)
 
 
 if __name__ == "__main__":
